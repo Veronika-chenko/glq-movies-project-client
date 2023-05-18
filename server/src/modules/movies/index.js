ae40fd2cacb219729c5e5457e8561153ac637807
Veronika Kravchenko <aniregma@gmail.com>
2023-05-18 20:48:10 +0300

    server for moviesByIds

--- server/src/modules/movies/index.js
+++ server/src/modules/movies/index.js
@@ -1,16 +1,23 @@
 const axios = require('axios');
 const { Movies } = require('./entities/Movies');
 
-const { API_KEY } = process.env;
+const { API_KEY, BASE_URL } = process.env;
 
 const getPopular = async (page) => {
   const res = await axios.get(
-    `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=${page}&sort_by=popularity.desc&api_key=${API_KEY}`
+    `${BASE_URL}discover/movie?include_adult=false&include_video=false&language=en-US&page=${page}&sort_by=popularity.desc&api_key=${API_KEY}`
   );
 
   return new Movies(res.data);
 };
 
+const getDetails = (movieId) => {
+  return axios.get(
+    `${BASE_URL}movie/${movieId}?api_key=${API_KEY}&language=en-US`
+  );
+};
+
 module.exports = {
   getPopular,
+  getDetails,
 };
