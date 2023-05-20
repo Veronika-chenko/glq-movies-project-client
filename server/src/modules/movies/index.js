3ef3c1e66e2b94f6eea70cd08abfdb5ba593a991
Veronika Kravchenko <aniregma@gmail.com>
2023-05-20 23:24:26 +0300

    server internationalization

--- server/src/modules/movies/index.js
+++ server/src/modules/movies/index.js
@@ -3,17 +3,18 @@ const { Movies } = require('./entities/Movies');
 
 const { API_KEY, BASE_URL } = process.env;
 
-const getPopular = async (page) => {
+const getPopular = async (page, lang) => {
+  // console.log('🚀 ~ lang:', lang);
   const res = await axios.get(
-    `${BASE_URL}discover/movie?include_adult=false&include_video=false&language=en-US&page=${page}&sort_by=popularity.desc&api_key=${API_KEY}`
+    `${BASE_URL}discover/movie?include_adult=false&include_video=false&language=${lang}&page=${page}&sort_by=popularity.desc&api_key=${API_KEY}`
   );
 
   return new Movies(res.data);
 };
 
-const getDetails = (movieId) => {
+const getDetails = (movieId, lang) => {
   return axios.get(
-    `${BASE_URL}movie/${movieId}?api_key=${API_KEY}&language=en-US`
+    `${BASE_URL}movie/${movieId}?api_key=${API_KEY}&language=${lang}`
   );
 };
 
